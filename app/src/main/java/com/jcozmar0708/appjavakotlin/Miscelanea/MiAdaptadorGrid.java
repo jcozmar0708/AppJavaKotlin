53e2848bd95113b0882a9adbf063befe23a6332d
José Manuel Cózar <josemanueltrafu@gmail.com>
2025-01-28 00:18:51 +0100

    App terminada

--- app/src/main/java/com/jcozmar0708/appjavakotlin/Miscelanea/MiAdaptadorGrid.java
+++ app/src/main/java/com/jcozmar0708/appjavakotlin/Miscelanea/MiAdaptadorGrid.java
@@ -1,17 +1,19 @@
 package com.jcozmar0708.appjavakotlin.Miscelanea;
 
-import android.content.ClipData;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.jcozmar0708.appjavakotlin.R;
-import com.jcozmar0708.appjavakotlin.databinding.ItemListBinding;
+import com.jcozmar0708.appjavakotlin.databinding.ItemGridBinding;
 
 import java.util.List;
 
@@ -27,30 +29,39 @@ public class MiAdaptadorGrid extends ArrayAdapter<Alumno> {
         this.alumnos = alumnos;
     }
 
+    public static class ViewHolder {
+        ImageView imageView;
+        TextView textViewID;
+        TextView textViewNombre;
+        TextView textViewMedia;
+    }
+
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        ItemListBinding binding;
-        View view;
+        ViewHolder holder;
 
         if (convertView == null) {
-            binding = ItemListBinding.inflate(LayoutInflater.from(context), parent, false);
-            view = binding.getRoot();
-            view.setTag(binding.getRoot());
+            convertView = LayoutInflater.from(context).inflate(layout, parent, false);
+            holder = new ViewHolder();
+            holder.imageView = convertView.findViewById(R.id.imageView);
+            holder.textViewID = convertView.findViewById(R.id.textViewID);
+            holder.textViewNombre = convertView.findViewById(R.id.textViewNombre);
+            holder.textViewMedia = convertView.findViewById(R.id.textViewMedia);
+            convertView.setTag(holder);
         } else {
-            binding = (ItemListBinding) convertView.getTag();
-            view = convertView;
+            holder = (ViewHolder) convertView.getTag();
         }
 
         Alumno alumnoActual = alumnos.get(position);
 
-        binding.textViewID.setText("ID: " + alumnoActual.getId());
-        binding.textViewNombre.setText(alumnoActual.getNombre());
-        binding.textViewMedia.setText("Media: " + alumnoActual.getMedia());
+        holder.textViewID.setText("ID: " + alumnoActual.getId());
+        holder.textViewNombre.setText(alumnoActual.getNombre());
+        holder.textViewMedia.setText("Media: " + alumnoActual.getMedia());
 
-        if (alumnoActual.getMedia() >= 5) binding.imageView.setImageResource(R.drawable.ic_aceptar);
-        else binding.imageView.setImageResource(R.drawable.ic_cancelar);
+        if (alumnoActual.getMedia() >= 5) holder.imageView.setImageResource(R.drawable.ic_aceptar);
+        else holder.imageView.setImageResource(R.drawable.ic_cancelar);
 
-        return view;
+        return convertView;
     }
 }
